992ef3aa0f829b4a11c5c6d8239e2b23a13952a0
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2023-12-27 19:34:17 +0200

    Update config.cjs

--- database/config.cjs
+++ database/config.cjs
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   actions: {
-    username: process.env.DB_HOST,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
