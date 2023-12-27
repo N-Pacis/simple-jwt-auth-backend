1f4d72b186f89f794c86bac954b6be4db397ef1e
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2023-12-27 18:13:33 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -15,22 +15,26 @@ if (process.env.GITHUB_ACTIONS) {
   db_user = String(process.env.GA_DB_USER);
   db_password = String(process.env.GA_DB_PASSWORD);
   db_port = String(process.env.GA_DB_PORT);
-} else if (environment === "PROD") {
-  db_host = String(process.env.PROD_DB_HOST);
-  db_name = String(process.env.PROD_DB_NAME);
-  db_user = String(process.env.PROD_DB_USER);
-  db_password = String(process.env.PROD_DB_PASSWORD);
-  db_port = String(process.env.PROD_DB_PORT);
-} else if (environment === "TEST") {
-  db_host = String(process.env.TEST_DB_HOST);
-  db_name = String(process.env.TEST_DB_NAME);
-  db_user = String(process.env.TEST_DB_USER);
-  db_password = String(process.env.TEST_DB_PASSWORD);
-  db_port = String(process.env.TEST_DB_PORT);
-} else {
-  db_host = String(process.env.DEV_DB_HOST);
-  db_name = String(process.env.DEV_DB_NAME);
-  db_user = String(process.env.DEV_DB_USER);
-  db_password = String(process.env.DEV_DB_PASSWORD);
-  db_port = String(process.env.DEV_DB_PORT);
 }
+else{
+  if (environment === "PROD") {
+    db_host = String(process.env.PROD_DB_HOST);
+    db_name = String(process.env.PROD_DB_NAME);
+    db_user = String(process.env.PROD_DB_USER);
+    db_password = String(process.env.PROD_DB_PASSWORD);
+    db_port = String(process.env.PROD_DB_PORT);
+  } else if (environment === "TEST") {
+    db_host = String(process.env.TEST_DB_HOST);
+    db_name = String(process.env.TEST_DB_NAME);
+    db_user = String(process.env.TEST_DB_USER);
+    db_password = String(process.env.TEST_DB_PASSWORD);
+    db_port = String(process.env.TEST_DB_PORT);
+  } else {
+    db_host = String(process.env.DEV_DB_HOST);
+    db_name = String(process.env.DEV_DB_NAME);
+    db_user = String(process.env.DEV_DB_USER);
+    db_password = String(process.env.DEV_DB_PASSWORD);
+    db_port = String(process.env.DEV_DB_PORT);
+  }
+} 
+
